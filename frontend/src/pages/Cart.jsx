6ecc6aaca86494995e73c4d4eb398d296faf222d
Mashhod Bin Shahid <mashhodbinshahid11@gmail.com>
2025-09-26 07:00:15 +0500

    Update Cart.jsx

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { ShoppingCart, Plus, Minus, Trash2, ArrowLeft } from 'lucide-react';
 import { useCart } from '../context/CartContext';
